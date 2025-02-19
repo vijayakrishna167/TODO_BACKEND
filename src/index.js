8067ffd2b96e10c6871fe617f618c2fa6be4fd5a
vijayakrishna167 <23mh5a4202@acoe.edu.in>
2025-02-19 10:37:23 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import connectDB from './DB/db.js';
 const app= express();
 import router from './route/uesrRoute.js';
 app.use(express.json());
+app.use(cors());
 // app.get('/',(req,res)=>{
 //     res.send("hello world");
 // })
